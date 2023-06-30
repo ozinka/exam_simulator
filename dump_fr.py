4f2ea615712627c1d0c24c0fed81f56c2eab6c75
vitaliy.osidach <vitaliy.osidach@ihsmarkit.com>
2023-06-30 12:20:40 +0300

    add on_change event for TextBox

--- dump_fr.py
+++ dump_fr.py
@@ -13,20 +13,19 @@ class DumpFrame(ctk.CTkFrame):
         self.fr_row_data = ctk.CTkFrame(self.fr_main)
         self.fr_row_data.pack(pady=5, padx=5, side=ctk.LEFT, anchor=ctk.N, fill=ctk.Y)
 
+        # Question TextBox
         self.lb_question = ctk.CTkLabel(self.fr_row_data, text="Question")
         self.lb_question.pack(anchor=ctk.W, padx=5)
 
-        # Question TextBox
         self.tb_question = ctk.CTkTextbox(self.fr_row_data, width=400)
         self.tb_question.pack(anchor=ctk.W, padx=5)
+        self.tb_question.bind("<<Modified>>", self.onModification)
 
+        # Answer TextBox
         self.lb_answers = ctk.CTkLabel(self.fr_row_data, text="Answers")
         self.lb_answers.pack(anchor=ctk.W, pady=(5, 0), padx=5)
 
-        # Answer TextBox
-        self.var_answer = ctk.StringVar()
-        self.var_answer.trace("w", lambda name, index, mode, var=self.var_answer: self.OnAnswerChange(self.var_answer))
-        self.tb_answers = ctk.CTkTextbox(self.fr_row_data, width=400, height=100, text=self.var_answer)
+        self.tb_answers = ctk.CTkTextbox(self.fr_row_data, width=400, height=100)
         self.tb_answers.pack(anchor=ctk.W, padx=5, pady=(0, 5), expand=True, fill=ctk.Y)
 
         # Frame for question example
@@ -38,12 +37,17 @@ class DumpFrame(ctk.CTkFrame):
                                         fg_color="#DA3633")
         self.btn_cancel.pack(side=ctk.BOTTOM, pady=30, padx=10)
 
+    def onModification(self, event):
+        flag = self.tb_question.edit_modified()
+        if flag:
+            print(event)
+            self.tb_question.edit_modified(False)
+
     def cancel(self):
         self.master.select_frame("MainFrame")
 
     def OnAnswerChange(self):
         print(self.var_answer.get())
-
 
 
 if __name__ == "__main__":
