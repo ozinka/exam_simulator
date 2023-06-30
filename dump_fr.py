fe4e46bad83a3cf36f3610cae4a09c75f6d6b654
vitaliy.osidach <vitaliy.osidach@ihsmarkit.com>
2023-06-30 22:20:59 +0300

    add example of question

--- dump_fr.py
+++ dump_fr.py
@@ -19,7 +19,7 @@ class DumpFrame(ctk.CTkFrame):
 
         self.tb_question = ctk.CTkTextbox(self.fr_row_data, width=400)
         self.tb_question.pack(anchor=ctk.W, padx=5)
-        self.tb_question.bind("<<Modified>>", self.onModification)
+        self.tb_question.bind("<<Modified>>", self.on_tb_answer_change)
 
         # Answer TextBox
         self.lb_answers = ctk.CTkLabel(self.fr_row_data, text="Answers")
@@ -32,22 +32,25 @@ class DumpFrame(ctk.CTkFrame):
         self.fr_row_data = ctk.CTkFrame(self.fr_main)
         self.fr_row_data.pack(pady=5, padx=(0, 5), side=ctk.LEFT, fill=ctk.BOTH, expand=True)
 
+        self.tb_question_ex = ctk.CTkTextbox(self.fr_row_data, state="disabled") #
+        self.tb_question_ex.pack(anchor=ctk.W, padx=5, fill=ctk.X)
+
         # Button Cancel
         self.btn_cancel = ctk.CTkButton(self, width=200, height=40, text="Cancel", command=self.cancel,
                                         fg_color="#DA3633")
         self.btn_cancel.pack(side=ctk.BOTTOM, pady=30, padx=10)
 
-    def onModification(self, event):
+    def on_tb_answer_change(self, event):
         flag = self.tb_question.edit_modified()
         if flag:
-            print(event)
+            self.tb_question_ex.configure(state="normal")
+            self.tb_question_ex.delete("0.0", "end")
+            self.tb_question_ex.insert("0.0", text=str(self.tb_question.get("0.0", "end")).strip())
             self.tb_question.edit_modified(False)
+            self.tb_question_ex.configure(state="disabled")
 
     def cancel(self):
         self.master.select_frame("MainFrame")
-
-    def OnAnswerChange(self):
-        print(self.var_answer.get())
 
 
 if __name__ == "__main__":
